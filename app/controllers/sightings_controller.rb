d8190a51f0518c5c4da6f6e3f38f32c3e9b3c2e6
Jim Padilla and Kevin Finley <jim+kevin@>
2017-02-06 13:34:30 -0800

    Add new and edit functionality for wildlife sightings

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,38 @@
-class SightingsController < AppicationController
+class SightingsController < ApplicationController
   def new
-    @sighting = Sighting.new
+    @wildlife = Wildlife.find(params[:wildlife_id])
+    @sighting = @wildlife.sightings.new
     render :new
+  end
+
+  def create
+    @wildlife = Wildlife.find(params[:wildlife_id])
+    @sighting = @wildlife.sightings.new(sighting_params)
+    if @sighting.save
+      redirect_to wildlife_path(@sighting.wildlife)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @wildlife = Wildlife.find(params[:wildlife_id])
+    @sighting = Sighting.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @wildlife = Wildlife.find(params[:wildlife_id])
+    @sighting = Sighting.find(params[:id])
+    if @sighting.update(sighting_params)
+      redirect_to wildlife_path(@sighting.wildlife)
+    else
+      render :edit
+    end
+  end
+
+private
+  def sighting_params
+    params.require(:sighting).permit(:date, :latitude, :longitude)
   end
 end

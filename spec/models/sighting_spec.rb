377dc8bfd3b0bb64a6b2623372a90f8932043323
Kevin Finley <kftwotwo@gmail.com>
2017-02-06 21:50:17 -0800

    fix specs for sighting to be correct

--- spec/models/sighting_spec.rb
+++ spec/models/sighting_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 describe Sighting do
-  it {
-    should validate_presence_of :date
-    should validate_presence_of :latitude
-    should validate_presence_of :longitude
-   }
+  it { should validate_presence_of :date }
+  it { should validate_presence_of :latitude }
+  it { should validate_presence_of :longitude }
 end
